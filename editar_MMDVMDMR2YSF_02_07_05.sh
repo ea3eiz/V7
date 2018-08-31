0dab4a382cb0d44515449c7e776afda0d466c8bb
ea3eiz <ea3eiz@ea3eiz.com>
2018-08-31 10:37:01 +0200

    1

--- editar_MMDVMDMR2YSF_02_07_05.sh
+++ editar_MMDVMDMR2YSF_02_07_05.sh
@@ -1198,7 +1198,7 @@ do
                           actualizar=S 
                           case $actualizar in
                           [sS]* ) echo ""         
-                          leafpad /home/pi/YSFClients/YSFGateway/YSFHosts.txt & sudo geany /home/pi/DMR2YSF/TG-YSFList.txt
+                          gedit /home/pi/YSFClients/YSFGateway/YSFHosts.txt & sudo geany /home/pi/DMR2YSF/TG-YSFList.txt
                           break;;
                           [nN]* ) echo ""
                           break;;
