380e99de1e8f7942244243750b95793946ad8bb3
ea3eiz <ea3eiz@ea3eiz.com>
2018-09-04 13:00:10 +0200

    1

--- editar_MMDVMDMR2YSF_02_07_05.sh
+++ editar_MMDVMDMR2YSF_02_07_05.sh
@@ -535,7 +535,7 @@ echo "Valor  actual  del Id: \33[1;33m${idd#*=}\33[1;37m"
                           case $actualizar in
 			                    [sS]* ) echo ""
 
-                          $numero_linea_idd=$numero_linea_idd$letra
+                          numero_linea_idd=$numero_linea_idd$letra
 
                           sed -i "3c Id=$miid" /home/pi/MMDVMHost/MMDVMDMR2YSF.ini
                           sed -i "$numero_linea_idd Id=$miid" /home/pi/DMR2YSF/DMR2YSF.ini
