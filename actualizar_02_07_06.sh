f0416a65398fab6971a5dae39831118b097ac11e
ea3eiz <ea3eiz@ea3eiz.com>
2018-09-11 10:10:46 +0200

    1

--- actualizar_02_07_06.sh
+++ actualizar_02_07_06.sh
@@ -222,5 +222,5 @@ cp /home/pi/$SCRIPTS_version/desconectar_MMDVMPLUS.php /var/www/html/panel_contr
 
 						sudo cp /home/pi/$SCRIPTS_version/Desktop/MENU_EXTRA /home/pi/Desktop
 
-indicativo=`sed -n '40p'  /home/pi/info_panel_control.ini`
+indicativo=`sed -n '2p'  /home/pi/MMDVMHost/TODOS_LOS_INIS.ini`
 sudo wget -post-data http://associacioader.com/prueba.php?mi_indicativo=$indicativo'&'callPLUS=$plus'&'masterBM=$rbm'&'masterPLUS=$rplus'&'version=$SCRIPTS_version
