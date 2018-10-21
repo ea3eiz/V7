baaf3c5e11a3664a8be08278f6e4c8bfad31407c
ea3eiz <ea3eiz@ea3eiz.com>
2018-10-22 00:46:06 +0200

    1

--- Copia-Restauracion_02_07_04.sh
+++ Copia-Restauracion_02_07_04.sh
@@ -8,15 +8,15 @@ VERDE="\033[1;32m"
 BLANCO="\033[1;37m"
 AMARILLO="\033[1;33m"
 echo "${VERDE}"
-echo "   ******************************************************************"
-echo "   *       Script para Copiar y Restaurar Configuraciones \33[1;33m\33[1;32m          *"
-echo "   *                          \33[1;31mby EA3EIZ\33[1;32m                             *"
-echo "   ******************************************************************"
+echo "   **************************************************************************"
+echo "   *       Script para Copiar y Restaurar Configuraciones \33[1;33m\33[1;32m                  *"
+echo "   *                          \33[1;31mby EA3EIZ\33[1;32m                                     *"
+echo "   **************************************************************************"
 echo "\33[1;36m   1)\33[1;37m Hacer Copia de seguridad de BM, DMR+, LIBRE, RADIO, solo D-STAR"
-echo "      solo FUSION, YSF2DMR, DMR2YSFF, DMR2NXDN, YSF, BlueDV y SVXLINK"
+echo "      solo FUSION, YSF2DMR, DMR2YSFF, DMR2NXDN, YSF, BlueDV, SVXLINK, ircDDB y D-STAR Repeater"
 echo ""
 echo "\33[1;36m   2)\33[1;32m Restaurar Copia de BM, DMR+, LIBRE, RADIO, solo D-STAR "
-echo "      solo FUSION, YSF2DMR, DMR2YSFF, DMR2NXDN, YSF, BlueDV y SVXLINK "
+echo "      solo FUSION, YSF2DMR, DMR2YSFF, DMR2NXDN, YSF, BlueDV, SVXLINK, ircDDB y D-STAR Repeater"
 echo ""
 echo "\33[1;36m   0)\33[1;34m Salir del script \33[1;31m OJO!! no salir con ctrl+c ni con la x"
 echo ""
@@ -179,6 +179,12 @@ sleep 3
             sudo cp -f svxlink.conf /usr/local/etc/svxlink/
             
             sudo cp -f ModuleEchoLink.conf /usr/local/etc/svxlink/svxlink.d/
+
+            cp ircddbgateway /usr/local/etc/opendv/
+            cp dstarrepeater /usr/local/etc/opendv/
+
+            cp info_panel_control.ini /home/pi/
+
 ;;
       *)
          clear
