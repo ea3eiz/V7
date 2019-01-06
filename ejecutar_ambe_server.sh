345e98a76bb53c0bf7b7e28fd05849d46e3ea4d8
ea3eiz <ea3eiz@ea3eiz.com>
2019-01-06 13:43:46 +0100

    1

--- ejecutar_ambe_server.sh
+++ ejecutar_ambe_server.sh
@@ -9,6 +9,7 @@ sed -i "11c AMBE_SERVER=ON" /home/pi/status.ini
 
 puerto_router=$(awk "NR==1" /home/pi/ambe_server.ini) 
 puerto_modem=$(awk "NR==2" /home/pi/ambe_server.ini)
+baut_rate=$(awk "NR==3" /home/pi/ambe_server.ini)
 cd /home/pi
 sudo cp abrir_ambe_server.desktop /home/pi/Desktop
 sleep 2
@@ -17,7 +18,7 @@ sudo rm /home/pi/abrir_ambe_server.desktop
 cd /home/pi/AMBEServer
 sudo killall AMBEserver
 sleep 1
-./AMBEserver -p $puerto_router -i $puerto_modem
+./AMBEserver -p $puerto_router -i $puerto_modem -s $baut_rate
 
 				
 						
