10c241d7e72ffdf1d10d9c4e9c957763f7e74c9c
ea3eiz <ea3eiz@ea3eiz.com>
2019-01-18 14:02:58 +0100

    1

--- actualiza_YSF_YSF2DMR_DMR2YSF_DMR2NXDN_NXDNClients.sh
+++ actualiza_YSF_YSF2DMR_DMR2YSF_DMR2NXDN_NXDNClients.sh
@@ -9,45 +9,45 @@ GRIS="\033[0m"
 
 
                         #comprueba si la versión a cambiado
-                        #version_YSFClients_actual=$(awk "NR==22" /home/pi/YSFClients/YSFGateway/Version.h)
-                        #version_YSFClients_instalada=$(awk "NR==1" /home/pi/versiones_YSF2DMR_DMR2YSF_DMR2NXDN.ini)
-                        #if [ "$version_YSFClients_actual" = "$version_YSFClients_instalada" ];
-                        # =================================
-                        #then 
-                        #clear
-                        #echo "************************************************************"
-                        #echo "*   YA TIENES LA ÚLTIMA VERSIÓN YSFClientes/YSFGateway     *"
-                        #echo "*   $version_YSFClients_actual                      *"
-                        #echo "************************************************************"
-                        #sleep 4
-                        #clear
-                        #else
-                        #sed -i "1c $version_YSFClients_actual" /home/pi/versiones_YSF2DMR_DMR2YSF_DMR2NXDN.ini 
-                        #echo "${VERDE}DESCARGANDO E INSTALANDO EL ÚLTIMO YSFClients${CIAN}"
-                        #sleep 3
-                        #sudo rm -R /home/pi/YSFClients
-                        #cd /home/pi
-                        #git clone https://github.com/g4klx/YSFClients
-                        #cd /home/pi/YSFClients/YSFGateway
-                        #make clean
-                        #make 
+                        version_YSFClients_actual=$(awk "NR==22" /home/pi/YSFClients/YSFGateway/Version.h)
+                        version_YSFClients_instalada=$(awk "NR==1" /home/pi/versiones_YSF2DMR_DMR2YSF_DMR2NXDN.ini)
+                        if [ "$version_YSFClients_actual" = "$version_YSFClients_instalada" ];
+                         =================================
+                        then 
+                        clear
+                        echo "************************************************************"
+                        echo "*   YA TIENES LA ÚLTIMA VERSIÓN YSFClientes/YSFGateway     *"
+                        echo "*   $version_YSFClients_actual                      *"
+                        echo "************************************************************"
+                        sleep 4
+                        clear
+                        else
+                        sed -i "1c $version_YSFClients_actual" /home/pi/versiones_YSF2DMR_DMR2YSF_DMR2NXDN.ini 
+                        echo "${VERDE}DESCARGANDO E INSTALANDO EL ÚLTIMO YSFClients${CIAN}"
+                        sleep 3
+                        sudo rm -R /home/pi/YSFClients
+                        cd /home/pi
+                        git clone https://github.com/g4klx/YSFClients
+                        cd /home/pi/YSFClients/YSFGateway
+                        make clean
+                        make 
 
-#echo "${VERDE}"
-#clear
-#echo "*************************************"
-#echo "* ACTUALIZANDO LISTADO DE SALAS YSF *"
-#echo "* ***********************************"
-#sleep 3
-                        #cd /home/pi/YSFClients/YSFGateway
-                        #sudo wget -O YSFHosts.txt http://register.ysfreflector.de/export_csv.php
-                        #sleep 3
-#clear
-#echo "${AMARILLO}"                      
-#echo "**********************************************************"
-#echo "* El fichero YSFHost.txt se ha actualizado correctamente *"
-#echo "**********************************************************"
-#sleep 3
-                        #fi
+echo "${VERDE}"
+clear
+echo "*************************************"
+echo "* ACTUALIZANDO LISTADO DE SALAS YSF *"
+echo "* ***********************************"
+sleep 3
+                        cd /home/pi/YSFClients/YSFGateway
+                        sudo wget -O YSFHosts.txt http://register.ysfreflector.de/export_csv.php
+                        sleep 3
+clear
+echo "${AMARILLO}"                      
+echo "**********************************************************"
+echo "* El fichero YSFHost.txt se ha actualizado correctamente *"
+echo "**********************************************************"
+sleep 3
+                        fi
 
 
 
