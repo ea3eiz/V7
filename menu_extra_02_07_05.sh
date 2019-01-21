652c7ba8c235db768470ccffb6c9e02175fd2793
ea3eiz <ea3eiz@ea3eiz.com>
2019-01-21 09:47:53 +0100

    Update menu_extra_02_07_05.sh

--- menu_extra_02_07_05.sh
+++ menu_extra_02_07_05.sh
@@ -49,11 +49,12 @@ clear
 
                         cd /usr/local/share/opendv/
                         sudo curl --fail -o DExtra_Hosts.txt -s http://www.pistar.uk/downloads/DExtra_Hosts.txt
+                        sudo curl --fail -o DCS_Hosts.txt -s http://www.pistar.uk/downloads/DCS_Hosts.txt
+                        sudo curl --fail -o DPlus_Hosts.txt -s http://www.pistar.uk/downloads/DPlus_Hosts.txt
 
                         cd /home/pi/$SCRIPTS_version
                         sudo git pull
                         sleep 1
-
 
                         #comprueba si el fichero existe
                         if [ -f /home/pi/versiones_YSF2DMR_DMR2YSF_DMR2NXDN.ini ];
