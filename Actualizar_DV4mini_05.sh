121bb3d6de84ebc2337476f01d0d8731eb2f0333
ea3eiz <ea3eiz@ea3eiz.com>
2019-03-10 21:53:22 +0100

    Update Actualizar_DV4mini_05.sh

--- Actualizar_DV4mini_05.sh
+++ Actualizar_DV4mini_05.sh
@@ -9,7 +9,7 @@ echo "   *                                                                   *"
 echo "   *********************************************************************"
 echo  "\33[1;36m   1)\33[1;33m Actualizar \33[1;32mDV4MINI20190307   \33[1;33m"
 echo  "\33[1;36m   2)\33[1;33m Volver a la versión \33[1;32mDV4MINI20190127   \33[1;33m"
-echo  "\33[1;36m   3)\33[1;33m Volver a la versión \33[1;32mDV4MINI20170517   \33[1;33m"
+#echo  "\33[1;36m   3)\33[1;33m Volver a la versión \33[1;32mDV4MINI20170517   \33[1;33m"
 echo ""
 echo "\33[1;36m   0)\33[1;34m Salir del script \33[1;31m OJO!! no salir con ctrl+c ni con la x"
 echo ""
@@ -85,7 +85,7 @@ clear
                         break;;
 esac
 done;;
-3) echo ""
+3bloqueado) echo ""
 while true
 do
 clear
