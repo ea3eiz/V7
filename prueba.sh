d0dc60a92af5fe7bc5dc4ee9e7a0729055c478af
ea3eiz <ea3eiz@ea3eiz.com>
2019-04-17 12:01:15 +0200

    Update prueba.sh

--- prueba.sh
+++ prueba.sh
@@ -2,6 +2,6 @@
 cd V7
 sudo git pull
 # Alarga los editores Solo Fusion y Solo D-STAR
-cp /home/pi/$SCRIPTS_version/Desktop/Editar_solofusion /home/pi/Desktop
-cp /home/pi/$SCRIPTS_version/Desktop/Editar_solodstar /home/pi/Desktop
-cp /home/pi/$SCRIPTS_version/Desktop/Editar_BM /home/pi/Desktop
+cp /home/pi/V7/Editar_solofusion /home/pi/Desktop
+cp /home/pi/V7/Desktop/Editar_solodstar /home/pi/Desktop
+cp /home/pi/V7/Desktop/Editar_BM /home/pi/Desktop
