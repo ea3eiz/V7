8e16b11808facc80c3354e5d15c95f17d3776171
ea3eiz <ea3eiz@ea3eiz.com>
2019-04-17 19:21:10 +0200

    Update md380tools_02_07_02.sh

--- md380tools_02_07_02.sh
+++ md380tools_02_07_02.sh
@@ -184,7 +184,7 @@ clear
                         #sudo wget http://42561.noip.us/user.bin
                         #sudo wget https://kg5rki.com/MD380_AIO/user.bin
                         sudo wget http://associacioader.com/descargas/user.bin
-                        sudo ./md380-tool spiflashwrite user.bin 0x100000
+                        sudo ./md380-tool spiflashwrite user.bin 0x1638400
                         break;;
                         [nN]* ) echo ""
                         echo ""
