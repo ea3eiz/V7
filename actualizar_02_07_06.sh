d97ee8a6564514f33884742cc1f5055f6ba1adce
ea3eiz <ea3eiz@ea3eiz.com>
2019-07-13 00:24:26 +0200

    Update actualizar_02_07_06.sh

--- actualizar_02_07_06.sh
+++ actualizar_02_07_06.sh
@@ -221,7 +221,6 @@ rbm=`sed -n '148p'  /home/pi/MMDVMHost/MMDVMBM.ini`
 rplus=`sed -n '148p'  /home/pi/MMDVMHost/MMDVMPLUS.ini`
 
 sudo cp /home/pi/$SCRIPTS_version/Desktop/Menu_Autoarranque /home/pi/Desktop
-sudo cp /home/pi/$SCRIPTS_version/Desktop/Abrir_LIBRE /home/pi/Desktop
 
 
 						#comprueba si el fichero existe
