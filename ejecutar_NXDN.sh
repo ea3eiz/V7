5b89e936c8d7c52058ec9b74485644e0ba42823a
ea3eiz <ea3eiz@ea3eiz.com>
2019-07-14 19:04:01 +0200

    Update ejecutar_NXDN.sh

--- ejecutar_NXDN.sh
+++ ejecutar_NXDN.sh
@@ -19,17 +19,17 @@
 #sleep 1
 #sudo rm /home/pi/RXF_DMR2NXDN.desktop
 
-#SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
-#cd /home/pi/Desktop
-#sudo cp Abrir_DMR2NXDN.desktop /home/pi
-#sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version;lxterminal --geometry=72x15 -e sudo sh cerrar_DMR2NXDN.sh'" /home/pi/Abrir_DMR2NXDN.desktop
-#sed -i "5c Icon=/home/pi/$SCRIPTS_version/DMR2NXDN_ON.png" /home/pi/Abrir_DMR2NXDN.desktop
-#sed -i "10c Name[es_ES]=Cerrar DMR2NXDN" /home/pi/Abrir_DMR2NXDN.desktop
-#sed -i "16c DMR2NXDN=ON" /home/pi/status.ini
-#cd /home/pi
-#sudo cp Abrir_DMR2NXDN.desktop /home/pi/Desktop
-#sleep 1
-#sudo rm /home/pi/Abrir_DMR2NXDN.desktop
+SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
+cd /home/pi/Desktop
+sudo cp Abrir_NXDN.desktop /home/pi
+sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version;lxterminal --geometry=72x15 -e sudo sh cerrar_NXDN.sh'" /home/pi/Abrir_DMR2NXDN.desktop
+sed -i "5c Icon=/home/pi/$SCRIPTS_version/DMR2NXDN_ON.png" /home/pi/Abrir_NXDN.desktop
+sed -i "10c Name[es_ES]=Cerrar NXDN" /home/pi/Abrir_NXDN.desktop
+sed -i "17c NXDN=ON" /home/pi/status.ini
+cd /home/pi
+sudo cp Abrir_NXDN.desktop /home/pi/Desktop
+sleep 1
+sudo rm /home/pi/Abrir_NXDN.desktop
 
 
 cd /home/pi/MMDVMHost
