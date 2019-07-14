fec13287a1a0a16a2aadea49076f4e01ed25b66b
ea3eiz <ea3eiz@ea3eiz.com>
2019-07-14 19:07:08 +0200

    Update ejecutar_NXDN.sh

--- ejecutar_NXDN.sh
+++ ejecutar_NXDN.sh
@@ -22,7 +22,7 @@
 SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
 cd /home/pi/Desktop
 sudo cp Abrir_NXDN.desktop /home/pi
-sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version;lxterminal --geometry=72x15 -e sudo sh cerrar_NXDN.sh'" /home/pi/Abrir_DMR2NXDN.desktop
+sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version;lxterminal --geometry=72x15 -e sudo sh cerrar_NXDN.sh'" /home/pi/Abrir_NXDN.desktop
 sed -i "5c Icon=/home/pi/$SCRIPTS_version/DMR2NXDN_ON.png" /home/pi/Abrir_NXDN.desktop
 sed -i "10c Name[es_ES]=Cerrar NXDN" /home/pi/Abrir_NXDN.desktop
 sed -i "17c NXDN=ON" /home/pi/status.ini
@@ -33,7 +33,7 @@ sudo rm /home/pi/Abrir_NXDN.desktop
 
 
 cd /home/pi/MMDVMHost
-sudo lxterminal --geometry=80x12 -e ./MMDVMNXDN MMDVMDMR2NXDN.ini &
+sudo lxterminal --geometry=80x12 -e ./MMDVMNXDN MMDVMNXDN.ini &
 
 cd /home/pi/NXDNClients/NXDNGateway
 sudo ./NXDNGateway NXDNGateway.ini
