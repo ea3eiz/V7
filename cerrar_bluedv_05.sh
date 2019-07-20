fca9e673c389a7f6cd0dcbf377c642b209190ba9
ea3eiz <ea3eiz@ea3eiz.com>
2019-07-20 17:21:50 +0200

    1

--- cerrar_bluedv_05.sh
+++ cerrar_bluedv_05.sh
@@ -3,7 +3,7 @@ SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
 cd /home/pi/Desktop
 sudo cp Abrir_BlueDV /home/pi
 sleep 1
-sed -i "4cExec=sh -c 'cd /home/pi/$SCRIPTS_version; sudo sh ejecutar_bluedv.sh'" /home/pi/Abrir_BlueDV
+sed -i "4cExec=sh -c 'cd /home/pi/$SCRIPTS_version; sudo sh ejecutar_bluedv_05.sh'" /home/pi/Abrir_BlueDV
 sed -i "5c Icon=/home/pi/$SCRIPTS_version/BLUEDV.png" /home/pi/Abrir_BlueDV
 sed -i "10c Name[es_ES]=Abrir BlueDV" /home/pi/Abrir_BlueDV
 sed -i "2c BlueDV=OFF" /home/pi/status.ini
