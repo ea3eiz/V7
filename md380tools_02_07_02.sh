17cdc6b082e96937cb90085005d1a27ef05f7f59
Manuel Díaz Jiménez <manel@iMac-de-Manuel.local>
2019-09-24 23:09:18 +0200

    Update md380tools_02_07_02.sh

--- md380tools_02_07_02.sh
+++ md380tools_02_07_02.sh
@@ -182,7 +182,7 @@ clear
                         sudo rm user.bin
                         echo "Introduciendo USER.BIN"
                         wget https://radioid.net/static/user.csv
-                        sudo mv user.csv user.bin
+                        
 
 
                         #sudo wget http://42561.noip.us/user.bin
