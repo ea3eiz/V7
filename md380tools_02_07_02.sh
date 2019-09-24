90c6042d0079eabef5b8d5f45466b90badecd805
Manuel Díaz Jiménez <manel@iMac-de-Manuel.local>
2019-09-25 00:09:29 +0200

    Update md380tools_02_07_02.sh

--- md380tools_02_07_02.sh
+++ md380tools_02_07_02.sh
@@ -183,7 +183,7 @@ clear
                         echo "Introduciendo USER.BIN"
                         wget --no-check-certificate https://radioid.net/static/user.csv
                         sudo mv user.csv user.bin
-                        sleep 1
+                        sleep 10
                         sudo ./md380-tool spiflashwrite user.bin 0x1638400
                         break;;
                         [nN]* ) echo ""
