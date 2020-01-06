e793377d9b4f126a25cbfe1e0321be3813eb2a9d
Manel <ea3eiz@ea3eiz.com>
2020-01-06 23:20:34 +0100

    Update editar_MMDVMBM_02_07_04.sh

--- editar_MMDVMBM_02_07_04.sh
+++ editar_MMDVMBM_02_07_04.sh
@@ -87,7 +87,7 @@ CIAN="\033[1;36m"
 GRIS="\033[0m"
 echo "${VERDE}"
 echo "   **************************************************************************"
-echo "             Script para Modificar $DIRECTORIO             \33[1;31m by EA3EIZ\33[1;32m   "
+echo "             xScript para Modificar $DIRECTORIO             \33[1;31m by EA3EIZ\33[1;32m   "
 echo "   **************************************************************************"
 echo -n "${CIAN}   1)${GRIS} Modificar indicativo  - ${AMARILLO}"
 ind=`grep -n "^Callsign=" /home/pi/MMDVMHost/$DIRECTORIO`
