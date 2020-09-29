aad51abe7b2b0b2fb829f969d08d13cb4ebefd6b
Manel <ea3eiz@ea3eiz.com>
2020-09-29 14:49:00 +0200

    1

--- actualizar_IDS_MMDVMHost.sh
+++ actualizar_IDS_MMDVMHost.sh
@@ -22,14 +22,6 @@ letrac=c
 linea_sed=$numero_linea$letrac
 sed -i "$linea_sed DATABASEURL='https://ham-digital.org/status/users.csv'" /home/pi/MMDVMHost/linux/DMRIDUpdate.sh
 
-# DMR IDs now served by RadioID.net
-var=`grep -n -m 1 '\<eval\>' /home/pi/MMDVMHost/linux/DMRIDUpdate.sh`
-buscar=":"
-largo_linea=`expr index $var $buscar`
-largo_linea=`expr $largo_linea - 1`
-numero_linea=`expr substr $var 1 $largo_linea`
-letrac=c
-linea_sed=$numero_linea$letrac
-sed -i "$linea_sed #eval ${RESTARTCOMMAND}" /home/pi/MMDVMHost/linux/DMRIDUpdate.sh
+
 
 sudo sh /home/pi/MMDVMHost/linux/DMRIDUpdate.sh
